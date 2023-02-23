98eaa14ce481976659b50ca275403e4779310306
alexandre deucher <alexandredeucher@gmail.com>
2023-02-23 10:04:05 -0300

    tela

--- pratica01/src/controle/PessoaDAO.java
+++ pratica01/src/controle/PessoaDAO.java
@@ -1,4 +1,5 @@
 package controle;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,37 +10,39 @@ import java.util.ArrayList;
 import modelo.Pessoa;
 
 public class PessoaDAO {
-	
+
 	private Conexao con;
-	
-	//insert
+
+	// insert
 	public boolean inserir(Pessoa p) {
-		
+
 		con = Conexao.getInstancia();
-		
 		Connection c = con.conectar();
+
 		try {
 			String query = "INSERT INTO pessoa (cpf, nome) VALLUES (?, ?);";
-			PreparedStatement stm = c.prepareStatement(null);
-			
-			stm.setInt(1, 123);
-			stm.setString(2, "alexandre");
-			
-			int valida = stm.executeUpdate();
+			PreparedStatement stm = c.prepareStatement(query);
+
+			stm.setInt(1, p.getCpf());
+			stm.setString(2, p.getNome());
+
+			stm.executeUpdate();
 			return true;
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		con.fecharConexao();
-		
+
 		return false;
 	}
-	//update
+
+	// update
 	public boolean atualizar(Pessoa p) {
-		Connection con = Conexao.getInstancia().conectar();
+		con = Conexao.getInstancia();
+		Connection c = con.conectar();
 		try {
 			String query = "UPDATE pessoa SET nome = ? WHERE cpf = ?";
-			PreparedStatement stm = con.prepareStatement(query);
+			PreparedStatement stm = c.prepareStatement(query);
 			stm.setString(1, p.getNome());
 			stm.setInt(2, p.getCpf());
 			stm.executeUpdate();
@@ -47,50 +50,57 @@ public class PessoaDAO {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+		con.fecharConexao();
+
 		return false;
 	}
-	//delete
-		public boolean deletar(Pessoa p) {
-			Connection con = Conexao.conectar();
-			try {
-				String query = "DELETE FROM pessoa WHERE cpf = ?";
-				PreparedStatement stm = con.prepareStatement(query);
-				stm.setInt(1, p.getCpf());
-				stm.executeUpdate();
-				return true;
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			Conexao.fecharConnection();
-			
-			return false;
-	}		
-	//listar
-		public ArrayList<Pessoa> listarPessoas(Pessoa p){
-			ArrayList<Pessoa> pessoas = new ArrayList<>();
-			
-			con = Conexao.getInstancia();
-			
-			Connection c = con.conectar();
+
+	// delete
+	public boolean deletar(Pessoa p) {
+		con = Conexao.getInstancia();
+		Connection c = con.conectar();
 		try {
-				
+			String query = "DELETE FROM pessoa WHERE cpf = ?";
+			PreparedStatement stm = c.prepareStatement(query);
+			stm.setInt(1, p.getCpf());
+			stm.executeUpdate();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		con.fecharConexao();
+
+		return false;
+	}
+
+	// listar
+	public ArrayList<Pessoa> listarPessoas() {
+		ArrayList<Pessoa> pessoas = new ArrayList<>();
+
+		con = Conexao.getInstancia();
+		Connection c = con.conectar();
+		try {
+
 			Statement stm = c.createStatement();
 			String query = "SELECT * FROM pessoa";
 			ResultSet rs = stm.executeQuery(query);
 			while (rs.next()) {
 				int cpf = rs.getInt("cpf");
 				String nome = rs.getString("nome");
+
+				Pessoa p = new Pessoa();
+				p.setCpf(cpf);
+				p.setNome(nome);
+
 				pessoas.add(p);
 			}
-			
-		}catch (SQLException e ) {
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-			con.fecharConexao();
-			
-		return null;	
-		}
+
+		con.fecharConexao();
+
+		return pessoas;
+	}
 }
